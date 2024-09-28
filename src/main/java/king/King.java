60452dbf7dcfaee2b542177d4b98848cc46b562e
Microzenas <microzenas@gmail.com>
2024-09-27 22:49:14 -0700

    king

---
+++ src/main/java/king/King.java
@@ -0,0 +1,12 @@
+package king;
+
+public class King {
+    private String kingdomName;
+    King(String kingdomName) {
+        this.kingdomName = kingdomName;
+    }
+
+    public String getKingdomName() {
+        return kingdomName;
+    }
+}
